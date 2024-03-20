407b8bb8d11d95ab9134e658dc4513b61835b2e0
ahmedhasan241 <92892508+ahmedhasan241@users.noreply.github.com>
2024-03-20 05:10:12 +0200

    Add files via upload

---
+++ src/services/api-client.ts
@@ -0,0 +1,37 @@
+import axios, { AxiosRequestConfig } from 'axios';
+
+export interface FetchResponse<T> {
+  count: number;
+  next: string | null;
+  results: T[];
+}
+
+const axiosInstance = axios.create({
+  baseURL: 'https://api.rawg.io/api',
+  params: {
+       key : '1a95d7a9ba964e189bc549069d806f99'
+  },
+});
+
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + '/' + id)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
